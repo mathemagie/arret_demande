e1fcca5a04e7f0534180621b7abd2b2bdc2e9da7
Fache Aurélien <aurelien.fache@gmail.com>
2014-01-15 15:31:59 +0100

    add error parsing code

--- get_data_bus.php
+++ get_data_bus.php
@@ -1,8 +1,11 @@
 <?php
 $cmd = "/usr/bin/w3m -dump 'http://wap.ratp.fr/siv/schedule?service=next&reseau=bus&lineid=B69&stationid=69_657_708' | grep -A 1 \">\" | sed '/--/d' | sed 's/> //' | sed 's/  //g'";
-//ligne 69 direction Gambetta
 exec($cmd,$res);
 $data_mn= $res[5];
+if (count($res) == 0) {//bug in parsing w3m command or wap http maybe non available
+        echo "2";
+        exit();
+}
 if ($_GET['debug']) {
         print_r($res);
         echo "<br/>";
