4394660a4f3a59757db65bdba4f28500afc87197
Fache Aurélien <aurelien.fache@gmail.com>
2014-01-15 14:41:21 +0100

    Update arret_demande.ino

--- arret_demande.ino
+++ arret_demande.ino
@@ -12,7 +12,7 @@ void loop() {
   // This command line runs the WifiStatus script, (/usr/bin/pretty-wifi-info.lua), then 
   // sends the result to the grep command to look for a line containing the word
   // "Signal:"  the result is passed to this sketch:
-  p.runShellCommand("/usr/bin/curl http://mathemagie.net/get_data_bus.php");
+  p.runShellCommand("/usr/bin/curl http://mathemagie.net/projects/arret_demande/get_data_bus.phpp");
 
   // do nothing until the process finishes, so you get the whole output:
   while(p.running());  
