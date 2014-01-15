b2faab6d1734be302f8a90865efc17c7b88bf72a
Fache Aurélien <aurelien.fache@gmail.com>
2014-01-15 15:30:52 +0100

    Update arret_demande.ino

--- arret_demande.ino
+++ arret_demande.ino
@@ -1,18 +1,21 @@
 #include <Process.h>
 #include <Bridge.h>
 
-const int ledPin = 13; 
+const int ledPin = 6; 
 
 void setup() {
   Bridge.begin();   // Initialize the Bridge
+   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
+  
+   
   Process p;
   // This command line runs the WifiStatus script, (/usr/bin/pretty-wifi-info.lua), then 
   // sends the result to the grep command to look for a line containing the word
   // "Signal:"  the result is passed to this sketch:
-  p.runShellCommand("/usr/bin/curl http://mathemagie.net/projects/arret_demande/get_data_bus.phpp");
+  p.runShellCommand("/usr/bin/curl http://mathemagie.net/projects/arret_demande/get_data_bus.php");
 
   // do nothing until the process finishes, so you get the whole output:
   while(p.running());  
@@ -28,8 +31,13 @@ void loop() {
       digitalWrite(ledPin, HIGH);
        delay(250);
       digitalWrite(ledPin, LOW);
-    }else {
-      digitalWrite(ledPin, LOW);
+    }
+     if (result == 2) {
+        digitalWrite(ledPin, HIGH);//bug in parsing process
+     }
+       
+    if (result == 0) {
+        digitalWrite(ledPin, LOW);//bug in parsing process
     }
   
     
