f9dfeae972d6d1a2e5e73b4b1ba79e124421e459
Fache Aurélien <aurelien.fache@gmail.com>
2014-04-01 18:47:12 +0200

    add console debug

--- arret_demande.ino
+++ arret_demande.ino
@@ -1,29 +1,28 @@
 #include <Process.h>
 #include <Bridge.h>
+#include <Console.h>
 
 const int ledPin = 6; 
 
 void setup() {
   Bridge.begin();   // Initialize the Bridge
-   pinMode(ledPin, OUTPUT);
+  Console.begin(); 
+  pinMode(ledPin, OUTPUT);
+ 
 }
 
 void loop() {
-  
-   
   Process p;
-  // This command line runs the WifiStatus script, (/usr/bin/pretty-wifi-info.lua), then 
-  // sends the result to the grep command to look for a line containing the word
-  // "Signal:"  the result is passed to this sketch:
   p.runShellCommand("/usr/bin/curl http://mathemagie.net/projects/arret_demande/get_data_bus.php");
-
-  // do nothing until the process finishes, so you get the whole output:
+  Console.println("run curl command");
+  
   while(p.running());  
 
   // Read command output. runShellCommand() should have passed "Signal: xx&":
   while (p.available()) {
     int result = p.parseInt();          // look for an integer
     if (result == 1) {
+      Console.println("light on");
       digitalWrite(ledPin, HIGH);
       delay(250);
       digitalWrite(ledPin, LOW);
@@ -38,6 +37,7 @@ void loop() {
        
     if (result == 0) {
         digitalWrite(ledPin, LOW);//bug in parsing process
+        Console.println("light off");
     }
   
     
